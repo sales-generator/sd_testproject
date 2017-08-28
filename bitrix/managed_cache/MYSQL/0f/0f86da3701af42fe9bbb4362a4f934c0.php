5f3f6d6cc78e820079f6a27ac8e8bcbed30f9060
Andrey <andreyromanov@salesgenerator.pro>
2017-08-28 11:01:25 +0300

    first commit

--- bitrix/managed_cache/MYSQL/0f/0f86da3701af42fe9bbb4362a4f934c0.php
+++ bitrix/managed_cache/MYSQL/0f/0f86da3701af42fe9bbb4362a4f934c0.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001503906862';
-$dateexpire = '001503906895';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:6:{s:7:"started";i:1503905458;s:6:"uptime";i:1404;s:8:"messages";i:0;s:11:"connections";i:0;s:15:"connections_now";i:0;s:7:"servers";a:0:{}}}';
+$datecreate = '001503907259';
+$dateexpire = '001503907292';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:6:{s:7:"started";i:1503905458;s:6:"uptime";i:1801;s:8:"messages";i:0;s:11:"connections";i:0;s:15:"connections_now";i:0;s:7:"servers";a:0:{}}}';
 return true;
 ?>
